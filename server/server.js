6f51d5d4a6c2ed16a35aadb21c470b4956b500e7
grahse <gyykim@gmail.com>
2020-12-11 01:29:40 -0800

    Create server route to get sellers bundle

--- server/server.js
+++ server/server.js
@@ -1,14 +1,24 @@
 const express = require('express');
 const path = require('path');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 
 const app = express();
 const PORT = 3000;
 
-app.use('/', express.static(path.join(__dirname, '../public')));
+app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('*', (req, res) => {
+app.use('/sellers/:id', createProxyMiddleware({
+  target: 'http://localhost:3002',
+  changeOrigin: true
+}));
+
+app.get('/:id', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
+
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
